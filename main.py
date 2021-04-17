bc1e65e120310b046910ae8c47f527489c6cc6d1
mathews <mathews2492@gmail.com>
2021-04-17 21:56:10 +0100

    made base files

---
+++ main.py
@@ -0,0 +1,13 @@
+COINBASE_API_CANDLESTICKS_URL = \
+    'https://api.pro.coinbase.com/products/{}/candles?granularity={}'
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
